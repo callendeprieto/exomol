739a161bc204c61dd7b1fb380aaeb78f2a73dfee
Carlos Allende <callende@iac.es>
2025-12-07 00:51:29 +0000

    updated list.f to correct a bug (Ivan's changed it)

--- xprog/merge.f
+++ xprog/merge.f
@@ -5,7 +5,7 @@ c
 c
       i=1
    10 continue
-      read(5,*,end=20) alam(i),anum(i),gf(i),excl(i),gr(i),gs(i),gw(i)
+      read(5,*,end=20) alam(i),anum(i),gf(i),excl(i)
       i=i+1
       go to 10
    20 continue
@@ -17,15 +17,14 @@ c
       inewend=0
       ioldend=0
    30 continue
-      read(10,*,end=50) al,anu,a,ex,r,s,w
-      write(*,*)'-->',al,anu,a,ex,r,s,w
+      read(10,*,end=50) al,anu,a,ex
       if(ioldend.eq.1) then
-         write(11,611) al,anu,a,ex,r,s,w
+         write(11,611) al,anu,a,ex
          j=j+1
          go to 30
       end if
       if(al.le.alam(k)) then
-         write(11,611) al,anu,a,ex,r,s,w
+         write(11,611) al,anu,a,ex
          j=j+1
        else
    40    continue
@@ -33,12 +32,11 @@ c
              write(6,621) k,n,alam(k),al,j,ioldend,inewend
   621        format(2i10,2f15.4,3i10)
             if(k.gt.n) go to 70
-            write(11,611) alam(k),anum(k),gf(k),excl(k),gr(k),gs(k),
-     *      gw(k)
+            write(11,611) alam(k),anum(k),gf(k),excl(k)
             k=k+1
             go to 40
           else
-            write(11,611) al,anu,a,ex,r,s,w
+            write(11,611) al,anu,a,ex
             j=j+1
          end if
       end if
@@ -47,20 +45,19 @@ c
       inewend=1
    60 continue
       if(k.gt.n) go to 70
-         write(11,611) alam(k),anum(k),gf(k),excl(k),gr(k),gs(k),gw(k)
+         write(11,611) alam(k),anum(k),gf(k),excl(k)
          k=k+1
          go to 60
    70 continue
       ioldend=1
       if(inewend.eq.0) then
-         write(11,611) al,anu,a,ex,r,s,w
+         write(11,611) al,anu,a,ex
          j=j+1
          go to 30
       end if
       write(*,*) 'number of lines in the second list',j
-  611 format(f10.4,f10.2,f8.3,f12.3,1p3e10.2)
+  611 format(f10.4,f10.2,f8.3,f12.3)
 c
       close(11)
-c
       end
  
